eba8e530fcecd7d1e618eb04562c45b8f20f14df
Lucana <k.ark.scarlet72@gmail.com>
2023-08-14 18:21:38 +0900

    [Finish]ウィンドウの枠組みを作成

--- WeekOfDay/WeekOfDay/Form1.Designer.cs
+++ WeekOfDay/WeekOfDay/Form1.Designer.cs
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelAD = new System.Windows.Forms.Label();
+            this.labelMonth = new System.Windows.Forms.Label();
+            this.labelDay = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.textBoxAD = new System.Windows.Forms.TextBox();
+            this.numericUpDownMonth = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownDay = new System.Windows.Forms.NumericUpDown();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelAD
+            // 
+            this.labelAD.AutoSize = true;
+            this.labelAD.Location = new System.Drawing.Point(25, 22);
+            this.labelAD.Name = "labelAD";
+            this.labelAD.Size = new System.Drawing.Size(41, 12);
+            this.labelAD.TabIndex = 0;
+            this.labelAD.Text = "西暦年";
+            // 
+            // labelMonth
+            // 
+            this.labelMonth.AutoSize = true;
+            this.labelMonth.Location = new System.Drawing.Point(156, 22);
+            this.labelMonth.Name = "labelMonth";
+            this.labelMonth.Size = new System.Drawing.Size(17, 12);
+            this.labelMonth.TabIndex = 1;
+            this.labelMonth.Text = "月";
+            // 
+            // labelDay
+            // 
+            this.labelDay.AutoSize = true;
+            this.labelDay.Location = new System.Drawing.Point(271, 22);
+            this.labelDay.Name = "labelDay";
+            this.labelDay.Size = new System.Drawing.Size(17, 12);
+            this.labelDay.TabIndex = 2;
+            this.labelDay.Text = "日";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(365, 45);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(35, 12);
+            this.labelResult.TabIndex = 3;
+            this.labelResult.Text = "label4";
+            // 
+            // textBoxAD
+            // 
+            this.textBoxAD.Location = new System.Drawing.Point(27, 38);
+            this.textBoxAD.Name = "textBoxAD";
+            this.textBoxAD.Size = new System.Drawing.Size(79, 19);
+            this.textBoxAD.TabIndex = 4;
+            this.textBoxAD.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // numericUpDownMonth
+            // 
+            this.numericUpDownMonth.Location = new System.Drawing.Point(158, 39);
+            this.numericUpDownMonth.Name = "numericUpDownMonth";
+            this.numericUpDownMonth.Size = new System.Drawing.Size(60, 19);
+            this.numericUpDownMonth.TabIndex = 5;
+            // 
+            // numericUpDownDay
+            // 
+            this.numericUpDownDay.Location = new System.Drawing.Point(273, 39);
+            this.numericUpDownDay.Name = "numericUpDownDay";
+            this.numericUpDownDay.Size = new System.Drawing.Size(60, 19);
+            this.numericUpDownDay.TabIndex = 6;
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(27, 98);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(373, 23);
+            this.buttonCalc.TabIndex = 7;
+            this.buttonCalc.Text = "曜日算出";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(478, 152);
+            this.Controls.Add(this.buttonCalc);
+            this.Controls.Add(this.numericUpDownDay);
+            this.Controls.Add(this.numericUpDownMonth);
+            this.Controls.Add(this.textBoxAD);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelDay);
+            this.Controls.Add(this.labelMonth);
+            this.Controls.Add(this.labelAD);
+            this.Name = "Form1";
+            this.Text = "曜日算出";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelAD;
+        private System.Windows.Forms.Label labelMonth;
+        private System.Windows.Forms.Label labelDay;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.TextBox textBoxAD;
+        private System.Windows.Forms.NumericUpDown numericUpDownMonth;
+        private System.Windows.Forms.NumericUpDown numericUpDownDay;
+        private System.Windows.Forms.Button buttonCalc;
     }
 }
 
