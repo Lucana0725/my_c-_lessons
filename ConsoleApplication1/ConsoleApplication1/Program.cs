f25448cb0a81183756a8a764b688562819c4a6dc
Lucana <k.ark.scarlet72@gmail.com>
2023-08-15 21:08:32 +0900

    [Finish]コンソールアプリケーションの作成開始

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
