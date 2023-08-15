2841b2cf402fc2ca574167bc6ad4ea681b48987f
Lucana <k.ark.scarlet72@gmail.com>
2023-08-15 23:18:01 +0900

    [Finish]while文の挙動を学習

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -13,19 +13,26 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            string ending = "終了するには何かキーを押してください。";
-
-            Console.Write("科目> ");
-            string subject = Console.ReadLine();
-            Console.Write("点数> ");
-            int score = int.Parse(Console.ReadLine());
-            Console.Write("平均点> ");
-            double average = double.Parse(Console.ReadLine());
-            Console.WriteLine("{0}は{1}点 (平均{2}点)", subject, score, average);
+            //string ending = "終了するには何かキーを押してください。";
 
 
-            Console.WriteLine(ending);
-            Console.ReadLine();
+            // 繰り返し処理
+            // while文
+            int n, sum = 0;
+
+            Console.Write("整数値> ");
+            n = int.Parse(Console.ReadLine());
+
+            while (n > 0)
+            {
+                sum += n;
+                Console.Write("整数値> ");
+                n = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("合計値は{0}です", sum);
+
+
+
         }
     }
 }
