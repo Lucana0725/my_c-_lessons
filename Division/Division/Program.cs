9d73102ac79ad2e65e43a26a15641ee6c6060e4f
Lucana <k.ark.scarlet72@gmail.com>
2023-08-16 11:24:05 +0900

    [Finish]継続確認のループを記述

--- Division/Division/Program.cs
+++ Division/Division/Program.cs
@@ -9,6 +9,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        string yn;
 
+        do
+        {
+            Console.WriteLine("処理を続けますか？ \'y\' / \'n\'");
+            yn = Console.ReadLine();
+        } while (yn == "y" || yn == "Y");
+
+        Console.WriteLine("終了するには何かキーを押してください。");
+        Console.Read();
     }
 }
