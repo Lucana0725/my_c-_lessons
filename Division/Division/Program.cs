b9d6033eac991e5424d201278bc5260a717c61b4
Lucana <k.ark.scarlet72@gmail.com>
2023-08-16 11:19:21 +0900

    [Finish]余りと商を求めるアプリの作成開始

---
+++ Division/Division/Program.cs
@@ -0,0 +1,14 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
