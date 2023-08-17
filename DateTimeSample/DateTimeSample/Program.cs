40bb846d7cb429597b71179d07774c094f0e6cff
Lucana <k.ark.scarlet72@gmail.com>
2023-08-17 11:51:15 +0900

    [Finish]不要な{}を削除

--- DateTimeSample/DateTimeSample/Program.cs
+++ DateTimeSample/DateTimeSample/Program.cs
@@ -12,28 +12,25 @@ namespace DateTimeSample
     {
         static void Main(string[] args)
         {
-            {
-                //Console.WriteLine("Hello world");
-                Console.WriteLine("今日の日付：" + DateTime.Today);
-                Console.WriteLine("現在の日付と時間：" + DateTime.Now);
+            //Console.WriteLine("Hello world");
+            Console.WriteLine("今日の日付：" + DateTime.Today);
+            Console.WriteLine("現在の日付と時間：" + DateTime.Now);
 
-                // 現在の二付と日時を取得する
-                DateTime now = DateTime.Now;
+            // 現在の二付と日時を取得する
+            DateTime now = DateTime.Now;
 
-                Console.WriteLine("年 = " + now.Year);
-                Console.WriteLine("月 = " + now.Month);
-                Console.WriteLine("日 = " + now.Day);
-                Console.WriteLine("時 = " + now.Hour);
-                Console.WriteLine("分 = " + now.Minute);
-                Console.WriteLine("秒 = " + now.Second);
-                Console.WriteLine("曜日 = " + now.DayOfWeek);
+            Console.WriteLine("年 = " + now.Year);
+            Console.WriteLine("月 = " + now.Month);
+            Console.WriteLine("日 = " + now.Day);
+            Console.WriteLine("時 = " + now.Hour);
+            Console.WriteLine("分 = " + now.Minute);
+            Console.WriteLine("秒 = " + now.Second);
+            Console.WriteLine("曜日 = " + now.DayOfWeek);
 
-                Console.WriteLine("Enterキーで終了");
-                Console.Read();
-            }
+            Console.WriteLine("Enterキーで終了");
+            Console.Read();
         }
     }
-
 }
 
 
