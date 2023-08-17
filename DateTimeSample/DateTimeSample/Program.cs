fe25f9f853cf472400c5f34543b4002f6aca5c13
Lucana <k.ark.scarlet72@gmail.com>
2023-08-17 12:02:30 +0900

    [Finish]現在時刻に任意の時間を加算する方法を学習

--- DateTimeSample/DateTimeSample/Program.cs
+++ DateTimeSample/DateTimeSample/Program.cs
@@ -1,40 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace DateTimeSample
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //Console.WriteLine("Hello world");
-            Console.WriteLine("今日の日付：" + DateTime.Today);
-            Console.WriteLine("現在の日付と時間：" + DateTime.Now);
-
-            // 現在の二付と日時を取得する
-            DateTime now = DateTime.Now;
-
-            Console.WriteLine("年 = " + now.Year);
-            Console.WriteLine("月 = " + now.Month);
-            Console.WriteLine("日 = " + now.Day);
-            Console.WriteLine("時 = " + now.Hour);
-            Console.WriteLine("分 = " + now.Minute);
-            Console.WriteLine("秒 = " + now.Second);
-            Console.WriteLine("曜日 = " + now.DayOfWeek);
-
-            Console.WriteLine("Enterキーで終了");
-            Console.Read();
-        }
-
-    }
-}
-
-
-
-// こちらの記述でも可
-//using System;
+﻿//using System;
 //using System.Collections.Generic;
 //using System.Linq;
 //using System.Threading.Tasks;
@@ -45,12 +9,6 @@ namespace DateTimeSample
 //    {
 //        static void Main(string[] args)
 //        {
-//            dateTimePropertyExample();
-//        }
-
-//        static void dateTimePropertyExample()
-//        {
-//            //Console.WriteLine("Hello world");
 //            Console.WriteLine("今日の日付：" + DateTime.Today);
 //            Console.WriteLine("現在の日付と時間：" + DateTime.Now);
 
@@ -71,5 +29,68 @@ namespace DateTimeSample
 
 //    }
 //}
+
+
+
+// こちらの記述でも可
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DateTimeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dateTimePropertyExample();
+            dateTimeAddExample();
+        }
+
+        static void ending()
+        {
+            Console.WriteLine("終了するには半角でEnterキーを押してください。");
+            Console.Read();
+        }
+
+        static void dateTimePropertyExample()
+        {
+            //Console.WriteLine("Hello world");
+            Console.WriteLine("今日の日付：" + DateTime.Today);
+            Console.WriteLine("現在の日付と時間：" + DateTime.Now);
+
+            // 現在の二付と日時を取得する
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine("年 = " + now.Year);
+            Console.WriteLine("月 = " + now.Month);
+            Console.WriteLine("日 = " + now.Day);
+            Console.WriteLine("時 = " + now.Hour);
+            Console.WriteLine("分 = " + now.Minute);
+            Console.WriteLine("秒 = " + now.Second);
+            Console.WriteLine("曜日 = " + now.DayOfWeek);
+
+            ending();
+        }
+
+        static void dateTimeAddExample()
+        {
+            DateTime now = DateTime.Now;                         // 現在の日付を得る
+            Console.WriteLine(now);                                   // 現在日時の表示
+
+            TimeSpan addSpan = new TimeSpan(10, 10, 10);  // TimeSpan (時、分、秒)
+            DateTime answer = now.Add(addSpan);              // TimeSpanの加算
+            Console.WriteLine(answer);
+
+            addSpan = new TimeSpan(1, 1, 1, 1);                // TimeSpan(日、時、分、秒)：要は現在時刻に10時間、10分、10秒を足した日時を出力する
+
+            //Console.WriteLine("Enterキーで終了");
+            //Console.Read();
+            ending();
+        }
+
+    }
+}
 
 
