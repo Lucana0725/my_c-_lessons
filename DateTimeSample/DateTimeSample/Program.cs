d7bae97d310275b72afe47fc48ab59fd88f3979b
Lucana <k.ark.scarlet72@gmail.com>
2023-08-17 11:54:50 +0900

    [Finish]二通りの書き方を練習

--- DateTimeSample/DateTimeSample/Program.cs
+++ DateTimeSample/DateTimeSample/Program.cs
@@ -1,7 +1,4 @@
-﻿//// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -30,39 +27,49 @@ namespace DateTimeSample
             Console.WriteLine("Enterキーで終了");
             Console.Read();
         }
+
     }
 }
 
 
 
-////// See https://aka.ms/new-console-template for more information
-////Console.WriteLine("Hello, World!");
-
+// こちらの記述でも可
 //using System;
 //using System.Collections.Generic;
 //using System.Linq;
 //using System.Threading.Tasks;
 
-////namespace DateTimeSample
-////{
-//    class Program
+//namespace DateTimeSample
 //{
-//    static void Main(string[] args)
+//    class Program
 //    {
-//        Console.WriteLine("今日の日付：" + DateTime.Today);
-//        Console.WriteLine("現在の日付と日時：" + DateTime.Now);
+//        static void Main(string[] args)
+//        {
+//            dateTimePropertyExample();
+//        }
 
-//        // 現在の日付と日時を取得する
-//        DateTime now = DateTime.Now;
+//        static void dateTimePropertyExample()
+//        {
+//            //Console.WriteLine("Hello world");
+//            Console.WriteLine("今日の日付：" + DateTime.Today);
+//            Console.WriteLine("現在の日付と時間：" + DateTime.Now);
 
-//        Console.WriteLine("年 = " + now.Year);
-//        Console.WriteLine("月 = " + now.Month);
-//        Console.WriteLine("日 = " + now.Day);
-//        Console.WriteLine("時 = " + now.Hour);
-//        Console.WriteLine("分 = " + now.Minute);
-//        Console.WriteLine("秒 = " + now.Second);
-//        Console.WriteLine("曜日 = " + now.DayOfWeek);
+//            // 現在の二付と日時を取得する
+//            DateTime now = DateTime.Now;
+
+//            Console.WriteLine("年 = " + now.Year);
+//            Console.WriteLine("月 = " + now.Month);
+//            Console.WriteLine("日 = " + now.Day);
+//            Console.WriteLine("時 = " + now.Hour);
+//            Console.WriteLine("分 = " + now.Minute);
+//            Console.WriteLine("秒 = " + now.Second);
+//            Console.WriteLine("曜日 = " + now.DayOfWeek);
+
+//            Console.WriteLine("Enterキーで終了");
+//            Console.Read();
+//        }
 
 //    }
 //}
-////}
+
+
