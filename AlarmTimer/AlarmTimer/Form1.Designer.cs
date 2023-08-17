5275f82433dbf1d482d7958baaaa6f8218a9eafa
Lucana <k.ark.scarlet72@gmail.com>
2023-08-17 14:45:59 +0900

    [Finish]例題：タイマー&アラームの作成スタート、枠組み作成完了

---
+++ AlarmTimer/AlarmTimer/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace AlarmTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonSet = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelTime.ForeColor = System.Drawing.Color.Yellow;
+            this.labelTime.Location = new System.Drawing.Point(41, 22);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(113, 37);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "label1";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.ForeColor = System.Drawing.Color.White;
+            this.labelDate.Location = new System.Drawing.Point(41, 62);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(123, 12);
+            this.labelDate.TabIndex = 1;
+            this.labelDate.Text = "YYYY年MM月DD日(曜)";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.ForeColor = System.Drawing.Color.Aqua;
+            this.labelStatus.Location = new System.Drawing.Point(41, 93);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(67, 12);
+            this.labelStatus.TabIndex = 2;
+            this.labelStatus.Text = "♪ hh:mm:ss";
+            // 
+            // buttonSet
+            // 
+            this.buttonSet.BackColor = System.Drawing.Color.MediumBlue;
+            this.buttonSet.ForeColor = System.Drawing.Color.White;
+            this.buttonSet.Location = new System.Drawing.Point(43, 130);
+            this.buttonSet.Name = "buttonSet";
+            this.buttonSet.Size = new System.Drawing.Size(75, 23);
+            this.buttonSet.TabIndex = 3;
+            this.buttonSet.Text = "設定";
+            this.buttonSet.UseVisualStyleBackColor = false;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.BackColor = System.Drawing.Color.MediumBlue;
+            this.buttonReset.ForeColor = System.Drawing.Color.White;
+            this.buttonReset.Location = new System.Drawing.Point(124, 130);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 4;
+            this.buttonReset.Text = "リセット";
+            this.buttonReset.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumBlue;
+            this.ClientSize = new System.Drawing.Size(327, 200);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonSet);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "アラーム&タイマー";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonSet;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
