98b20ac889d80e1069551bef2ea679868061d693
Lucana <k.ark.scarlet72@gmail.com>
2023-08-17 11:48:33 +0900

    [Finish]DateTime構造の理解用コンソールアプリの作成

---
+++ DateTimeSample/DateTimeSample/Program.cs
@@ -0,0 +1,67 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        {
+            //Console.WriteLine("Hello world");
+            Console.WriteLine("今日の日付：" + DateTime.Today);
+            Console.WriteLine("現在の日付と時間：" + DateTime.Now);
+
+            // 現在の二付と日時を取得する
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine("年 = " + now.Year);
+            Console.WriteLine("月 = " + now.Month);
+            Console.WriteLine("日 = " + now.Day);
+            Console.WriteLine("時 = " + now.Hour);
+            Console.WriteLine("分 = " + now.Minute);
+            Console.WriteLine("秒 = " + now.Second);
+            Console.WriteLine("曜日 = " + now.DayOfWeek);
+
+            Console.WriteLine("Enterキーで終了");
+            Console.Read();
+        }
+    }
+}
+
+
+
+////// See https://aka.ms/new-console-template for more information
+////Console.WriteLine("Hello, World!");
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+////namespace DateTimeSample
+////{
+//    class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("今日の日付：" + DateTime.Today);
+//        Console.WriteLine("現在の日付と日時：" + DateTime.Now);
+
+//        // 現在の日付と日時を取得する
+//        DateTime now = DateTime.Now;
+
+//        Console.WriteLine("年 = " + now.Year);
+//        Console.WriteLine("月 = " + now.Month);
+//        Console.WriteLine("日 = " + now.Day);
+//        Console.WriteLine("時 = " + now.Hour);
+//        Console.WriteLine("分 = " + now.Minute);
+//        Console.WriteLine("秒 = " + now.Second);
+//        Console.WriteLine("曜日 = " + now.DayOfWeek);
+
+//    }
+//}
+////}
