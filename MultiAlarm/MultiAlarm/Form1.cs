251aedcd94d437aaa36d057ef60cb14d431e266d
Lucana <k.ark.scarlet72@gmail.com>
2023-08-18 16:06:13 +0900

    [Finish]設定ウィンドウをOKで閉じたときにForm1のチェックボックスのテキストを設定時刻に、チェックボックスをチェックする処理を実装

--- MultiAlarm/MultiAlarm/Form1.cs
+++ MultiAlarm/MultiAlarm/Form1.cs
@@ -12,6 +12,12 @@ namespace MultiAlarm
 {
     public partial class Form1 : Form
     {
+        // 各種フィールド
+        private bool alarmSetFlag = false;  // アラームをセットしているかどうかの判定フラグ
+        private int alarmHour = 0;            // アラーム時
+        private int alarmMinute = 0;         // アラーム分
+        private int alarmSecond = 0;        // アラーム秒
+
         public Form1()
         {
             InitializeComponent();
@@ -38,7 +44,19 @@ namespace MultiAlarm
         private void buttonSet1_Click(object sender, EventArgs e)
         {
             Form2 form2 = new Form2();  // 設定ウィンドウのインスタンス作成
-            form2.ShowDialog();
+            //form2.ShowDialog();
+
+            // 設定ウィンドウがOKで閉じられた場合の処理
+            if (form2.ShowDialog() == DialogResult.OK)
+            {
+                // アラームの設定
+                alarmSetFlag = true;  // アラームをセットしたのでフラグをtrueに。
+                alarmHour = form2.alarmHour;
+                alarmMinute = form2.alarmMinute;
+                alarmSecond = form2.alarmSecond;
+                checkBoxTimer1.Text = alarmHour.ToString("00") + ":" + alarmMinute.ToString("00");
+                checkBoxTimer1.Checked = true;
+            }
             form2.Dispose();
         }
     }
