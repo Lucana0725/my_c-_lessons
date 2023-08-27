0d51dcae4c5bccb385eb4aef1e9ca267d74d0c37
Lucana <k.ark.scarlet72@gmail.com>
2023-08-27 23:34:07 +0900

    [Finish]Televisionクラスにデータメンバーを定義

--- ClassSample/ClassSample/Television.cs
+++ ClassSample/ClassSample/Television.cs
@@ -8,6 +8,11 @@ namespace ClassSample
 {
     internal class Television
     {
-        
+        // メンバー
+        public bool power;                      // 電源の状態
+        public int channel;                      // 現在のチャンネル
+        public int volume;                       // 現在の音量
+        public const int channelMin = 1;    // チャンネルの下限
+        public const int channelMax = 12;  // チャンネルの上限
     }
 }
