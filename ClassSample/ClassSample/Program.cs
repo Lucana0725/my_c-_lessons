6a0772509d1ecc3f8c6b8bf5a84ee192bf0757c0
Lucana <k.ark.scarlet72@gmail.com>
2023-08-27 23:33:10 +0900

    [Finish]ClassSample作成開始、Televisionクラスを新規作成

---
+++ ClassSample/ClassSample/Program.cs
@@ -0,0 +1,35 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using ClassSample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello world");
+        //Console.Read();
+
+        Television tv = new Television();
+
+        Console.WriteLine("10チャンネルを設定します");
+        tv.SetChannel(10);
+        Console.WriteLine("TVは{0}チャンネルです。", tv.channel);
+
+        Console.WriteLine("チャンネルを+2します。");
+        tv.ChannelUp();
+        tv.ChannelUp();
+        Console.WriteLine("TVは{0}チャンネルです。", tv.channel);
+
+        Console.WriteLine("チャンネルを-1します。");
+        tv.ChannelDown();
+        Console.WriteLine("TVは{0}チャンネルです。", tv.channel);
+
+        Console.WriteLine("終了するにはEnterキーを押してください。");
+        Console.Read();
+    }
+}
