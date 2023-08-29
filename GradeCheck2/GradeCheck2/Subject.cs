affb20b565f33dcbd9f0420ac06d8faf58df2db0
Lucana <k.ark.scarlet72@gmail.com>
2023-08-29 14:01:12 +0900

    [Finish]Subject.csクラスを新規作成、各種フィールドを定義

---
+++ GradeCheck2/GradeCheck2/Subject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCheck2
+{
+    internal class Subject
+    {
+        // フィールド
+        private double average;     // 平均点
+        private int totalHours;       // 総時数
+        private int passScore;       // 合格点
+    }
+}
