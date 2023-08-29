7148f09e9aee4256ca4cdc587a145f2c9f7bf9b2
Lucana <k.ark.scarlet72@gmail.com>
2023-08-29 12:00:09 +0900

    [Finish]得点、出席時数の入力値が正しいかどうかを判定するメソッド2つを定義

--- GradeCheck2/GradeCheck2/Student.cs
+++ GradeCheck2/GradeCheck2/Student.cs
@@ -18,7 +18,47 @@ namespace GradeCheck2
         private int attendanceE;        // 英語の出席時数
 
 
+
         // コンストラクタ
         // ここに定義予定
+
+
+
+        // プロパティ
+        // ここに定義予定
+
+
+
+
+        // 各種メソッド
+
+        // 得点の値が正しいかチェックする
+        private int scoreCheck(int val)
+        {
+            if (val >= 0 && val <= 100)
+            {
+                return val;
+            }
+            else  // 得点値が0より小さかったり100より大きかったら、メッセージボックスにて警告し、0を返す(入力値を反映しない)。
+            {
+                System.Windows.Forms.MessageBox.Show("得点の入力が正しくありません。", "確認");
+                return 0;
+            }
+        }
+
+        // 出席時数の値が正しいかチェックする
+        private int attendanceCheck(int val)
+        {
+            if (val >= 0)
+            {
+                return val;
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("出席時数の入力が正しくありません。", "確認");
+                return 0;
+            }
+        }
+
     }
 }
