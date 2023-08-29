4286cf138043e9b9a3ef7f5e356845703ebf8850
Lucana <k.ark.scarlet72@gmail.com>
2023-08-29 11:45:56 +0900

    [Finish]Studentクラスにおいて、必要な各種フィールドを定義

--- GradeCheck2/GradeCheck2/Student.cs
+++ GradeCheck2/GradeCheck2/Student.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace GradeCheck2
 {
+    // 学生クラス
     internal class Student
     {
+        // フィールド
+        private int scoreM;                 // 数学の点数
+        private int scoreP;                 // 物理の点数
+        private int scoreE;                 // 英語の点数
+        private int attendanceM;        // 数学の出席時数
+        private int attendanceP;        // 物理の出席時数
+        private int attendanceE;        // 英語の出席時数
+
+
+        // コンストラクタ
+        // ここに定義予定
     }
 }
