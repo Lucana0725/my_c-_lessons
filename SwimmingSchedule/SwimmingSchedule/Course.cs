0d3eb4d0e608d3b15783bd3fd4f5022da519b43e
Lucana <k.ark.scarlet72@gmail.com>
2023-08-30 11:34:47 +0900

    [Finish]Course.csにて各種フィールドを定義

--- SwimmingSchedule/SwimmingSchedule/Course.cs
+++ SwimmingSchedule/SwimmingSchedule/Course.cs
@@ -8,5 +8,33 @@ namespace SwimmingSchedule
 {
     internal class Course
     {
+        // -----フィールド-----
+        private string name;           // コース名
+        private string dayOfWeek;   // 曜日
+        private int startHour;         // 開始時間
+        private int fee;                 // 一回分の料金
+
+
+
+
+
+        // -----コンストラクタ-----
+        // ここに定義予定
+
+
+
+
+
+
+        // -----プロパティ-----
+        // ここに定義予定
+
+
+
+
+
+
+        // -----メソッド-----
+        // ここに定義予定
     }
 }
