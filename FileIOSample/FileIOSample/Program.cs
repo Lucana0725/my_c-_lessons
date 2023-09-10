3e21b8d5bd1ca3d4a4d078029fc30c4e8a02218e
Lucana <k.ark.scarlet72@gmail.com>
2023-09-10 18:24:44 +0900

    [Finish]ファイルの書き込み処理を学習

---
+++ FileIOSample/FileIOSample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 文字列を全部書き込む例
+            string writeText = "文字列をファイルに書き込みました。";
+
+            try
+            {
+                using (StreamWriter writer1 = new StreamWriter("test1.txt", false, Encoding.Default))
+                {
+                    writer1.Write(writeText);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            // 複数の文字列を1行ずつ書き込む
+            string[] writeText2 = { "続けて文字列を", "改行しながら", "書き込みます。" };
+
+            try
+            {
+                using (StreamWriter writer2 = new StreamWriter("test1.txt", true, Encoding.Default))
+                {
+                    foreach (string s in writeText2)
+                    {
+                        writer2.WriteLine(s);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
