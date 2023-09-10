5f34bc8d6811746fcc1be012033e2c9bae7f6bc1
Lucana <k.ark.scarlet72@gmail.com>
2023-09-10 17:47:30 +0900

    [Finish]フィールド、年月を保持するプロパティの定義

--- SwimmingSchedule/SwimmingSchedule/Form1.cs
+++ SwimmingSchedule/SwimmingSchedule/Form1.cs
@@ -12,27 +12,53 @@ namespace SwimmingSchedule
 {
     public partial class Form1 : Form
     {
+        // 本日の日付を取得する
+        DateTime now = DateTime.Now;
+
+        // フィールド
+        private int year;     // 年
+        private int month;  // 月
+
+
+
+
+        // プロパティ
+        public int Year
+        {
+            get { return year; }
+            set { year = (int)numericUpDownYear.Value; }
+        }
+
+        public int Month
+        {
+            get { return month; }
+            set { month = (int)numericUpDownMonth.Value; }
+        }
+
+
+
         public Form1()
         {
             InitializeComponent();
         }
 
 
-        // 本日の日付を取得する
-        //DateTime now = new DateTime();
-        DateTime now = DateTime.Now;
-
-
-
-
-
         // Form1ロード時のイベントハンドラ
-
         private void Form1_Load(object sender, EventArgs e)
         {
             // フォーム起動時に現在の年と月をニューメリックに表示する
             numericUpDownYear.Value = now.Year;
             numericUpDownMonth.Value = now.Month;
+        }
+
+
+
+
+
+        // 入力された年月の日数を求める
+        private void CalcDaysInMonth()
+        {
+            int daysInMonth = DateTime.DaysInMonth(Year, Month);
         }
     }
 }
